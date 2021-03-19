7de76d15d5b548ddf57e930b33d0d17ba1dec66c
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-19 12:51:12 +0000

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,19 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="sm:hidden flex items-center fixed bottom-0">
-      <div className='mb-0'>
+      <div className='relative'>
         <Image
         src="/footer/base-footer.svg"
         height={75}
         width={411} />
+      </div>
+      <div className='absolute inline-block'>
+          <button className='mx-auto'>
+          <Image
+        src="/assets/joaninha.svg"
+        height={33}
+        width={43} />
+          </button>
       </div>
     </div>
   );
