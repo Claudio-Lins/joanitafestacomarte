bb7dab420b2706503d99efb611b427de1c8befcc
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-20 18:02:08 +0000

    Quem Somos

--- pages/galeria.js
+++ pages/galeria.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Galeria = () => {
     return (
-        <div className='text-5xl text-red-600 mt-16 font-Lobster'>
+        <div className='text-5xl text-red-600 font-Lobster'>
             <h1>Galeria de fotos</h1>
         </div>
     );
