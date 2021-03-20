9a4a9d73a27230c6ffc746d370f8d907d4d6ac58
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-20 18:08:36 +0000

    Update quem-somos.js

--- pages/quem-somos.js
+++ pages/quem-somos.js
@@ -17,17 +17,17 @@ const QuemSomos = () => {
         </h2>
       </div>
       <div className="px-8 py-4">
-        <p className='text-xs font-Poppins font-extrabold leading-4 tracking-wide mb-20'>
+        <p className='text-xs font-Poppins font-extrabold leading-6 tracking-wide mb-20'>
           Eu sou a Karen, a cara por trás da Juanita. Sou formada em
           Publicidade, mas desde criança sempre gostei muito de artes manuais e
           depois de ser mãe mergulhei no universo das festas e me encantei. E
           assim surgiu a Joanita, onde me expresso e me realizo com cada festa,
           cada trabalho. Cada festa é personalizada para aquela ocasião e sempre
           feita com muito carinho e cuidado (pelas mãos de uma escorpiana
-          perfeccionista).⁣ Me encantei mais ainda com a possibilidade de uma
+          perfeccionista).⁣ <br/><br/>Me encantei mais ainda com a possibilidade de uma
           festa bonita e personalizada aliada à praticidade, pois como mãe sei
           que precisamos de coisas práticas, mas também queremos tudo lindo e
-          especial para os nossos filhos. Assim surgiu o carro chefe da Joanita
+          especial para os nossos filhos.<br/><br/> Assim surgiu o carro chefe da Joanita
         </p>
       </div>
     </div>
