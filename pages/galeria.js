66c7c05b279b530ac22cb29b736a341829de253b
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-20 16:44:18 +0000

    Custon Fonts
    Lobster
    Nunito
    Poppins

--- pages/galeria.js
+++ pages/galeria.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Galeria = () => {
     return (
-        <div className='text-5xl text-red-600 mt-16 font-serif'>
+        <div className='text-5xl text-red-600 mt-16 font-Lobster'>
             <h1>Galeria de fotos</h1>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Lobster: ['Lobster Two'],
+        Poppins: ['Poppins'],
+        Nunito: ['Nunito']
+      }
+    },
   },
   variants: {
     extend: {},
