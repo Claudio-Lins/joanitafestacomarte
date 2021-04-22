a5b2484d85f185272812b0ec59e6a596343457c8
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-04-22 23:13:20 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home({destak}) {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       
-      <div className='w-full shadow-lg rounded-md md:w-2/3 lg:w-[1280px] lg:h-[700px]'>
+      <div className='w-full shadow-xl rounded-md md:w-2/3 lg:w-[1280px] lg:h-[700px]'>
       <SliderDestak destak={destak} />
       </div>
       <div className='w-full h-[800px] flex justify-center items-center bg-red-200 my-10'>
