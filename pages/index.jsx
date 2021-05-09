ce647b79172525b64a071b2dae9dada170cf76e6
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-05-09 19:18:57 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Image from "next/image";
 import Head from "next/head";
 import DepoimentosSlideMobile from "../components/DepoimentosSlideMobile";
 import DestakHome from "../components/DestakHome";
