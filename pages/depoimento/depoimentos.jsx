a85496fe7a9f7b7482480614452038dbe3106753
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-05-10 21:58:59 +0100

    Create depoimentos.jsx

---
+++ pages/depoimento/depoimentos.jsx
@@ -0,0 +1,86 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Depoimentos() {
+  return (
+    <>
+      <Head>
+        <title>Depopimentos - Joanita Festa com Arte</title>
+      </Head>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 pt-20 min-h-screen">
+        {/*  */}
+        <div className="bg-red-800 lg:min-h-screen flex lg:items-center p-8 sm:p-12">
+          <div className="flex-grow text-white text-center">
+            <h1 className="text-5xl font-Lobster">Depoimentos</h1>
+            <p className=" font-extralight mt-3 tracking-wide">
+              Me conta aqui a sua opnião sobre a Joanita Festa com Arte.
+            </p>
+          </div>
+        </div>
+        {/*  */}
+        {/*  */}
+        <div className="lg:flex min-h-screen flex-col  px-6 md:px-12 lg:px-24 xl:p-48">
+          <div className="mx-auto text-center text-red-800 hidden md:flex">
+            <Image
+              src="/assets/joaninha-512.svg"
+              alt="Joanita Festa com Arte"
+              width={300}
+              height={100}
+            />
+          </div>
+          <div className="py-4">
+            <form className="w-full space-y-2 md:space-y-4">
+              <div className="flex items-center justify-between pl-3 border border-gray-200 rounded-lg shadow-sm overflow-hidden">
+                <input
+                  type="text"
+                  name="nome"
+                  className="text-sm font-light outline-none h-12 w-11/12 text-red-800 placeholder-red-800"
+                  placeholder="Nome"
+                />
+              </div>
+
+              <div className="flex items-center justify-between pl-3 border border-gray-200 rounded-lg shadow-sm overflow-hidden">
+                <input
+                  type="email"
+                  name="email"
+                  className="text-sm font-light outline-none h-12 w-11/12 text-red-800 placeholder-red-800"
+                  placeholder="E-mail"
+                />
+              </div>
+
+              <div className="">
+              <div className=' pl-3 text-red-800 text-sm'>Enviar Foto</div>
+              <div className="flex  pl-3 pt-3 pb-0 border border-gray-200 rounded-lg shadow-sm overflow-hidden">
+                <input
+                  type="file"
+                  name="email"
+                  className="text-sm font-light outline-none h-12 w-11/12 text-red-800 placeholder-red-800"
+                />
+              </div>
+              </div>
+
+              <div className="flex items-center justify-between pl-3 border border-gray-200 rounded-lg shadow-sm overflow-hidden">
+                <textarea
+                  name="mensage"
+                  className="text-sm font-light outline-none h-24 w-full py-2 text-red-800 placeholder-red-800"
+                  placeholder="Mensagem"
+                />
+              </div>
+
+              <div className="bg-red-800 rounded-lg py-2">
+                <button
+                  className="w-full text-base font-Nunito text-white tracking-wider hover:font-bold hover:text-xl"
+                  type="button"
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        {/*  */}
+      </div>
+    </>
+  );
+}
