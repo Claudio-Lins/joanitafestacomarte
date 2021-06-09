424c207f8479615450b614af5893d0b3aa398c78
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-09 20:13:47 +0100

    Update slideDestak.js

--- components/Destak/slideDestak.js
+++ components/Destak/slideDestak.js
@@ -10,13 +10,13 @@ const SlideDestak = ({ image }) => {
         transitionDuration: 600,
         infinite: true,
         canSwipe: true,
-        prevArrow: <div className='hidden md:block' style={{width: "30px", marginRight: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#991B1B"><path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z"/></svg></div>,
-        nextArrow: <div className='hidden md:block' style={{width: "30px", marginLeft: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#991B1B"><path d="M512 256L270 42.6v138.2H0v150.6h270v138z"/></svg></div>
+        prevArrow: <div className='hidden' style={{width: "30px", marginRight: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#991B1B"><path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z"/></svg></div>,
+        nextArrow: <div className='hidden' style={{width: "30px", marginLeft: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#991B1B"><path d="M512 256L270 42.6v138.2H0v150.6h270v138z"/></svg></div>
       };
   return (
     <>
-    <div className=" container w-[340px] md:w-[600px] mx-auto bg-black pb-4 pt-0">
-        <Slide {...properties} className="border-black border-4">
+    <div className=" container w-[340px] md:w-[600px] mx-auto bg-white py-4 rounded-xl shadow-lg">
+        <Slide {...properties} className="">
         {image.map((image, index) => {
             return (
             <>
@@ -31,7 +31,7 @@ const SlideDestak = ({ image }) => {
                     objectFit="cover"
                   />
                 </div>
-                <div className="text-white mt-2 text-center text-3xl tracking-wider font-Lobster">
+                <div className="text-black my-2 text-center text-3xl tracking-wider font-Lobster">
                   <h2>{image.Title}</h2>
                 </div>
               </div>
